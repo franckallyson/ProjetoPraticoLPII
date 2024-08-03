57fb221feebaf1df901a5d013b97b00ae3d84de9
franc <franckallyson@hotmail.com>
2024-08-03 11:06:41 -0300

    [Franck] Adição de Classe Produto para armazenamento de lista de produtos em memória

---
+++ SupermercadoIfba/src/Produto.java
@@ -0,0 +1,44 @@
+public class Produto {
+    int codigo;
+    String descricao;
+    double preco;
+
+    public Produto(int codigo, String descricao, double preco) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: {" +
+                "\n  codigo:" + codigo +
+                ",\n  descricao:'" + descricao + '\'' +
+                ",\n  preco:" + preco +
+                "\n}";
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
